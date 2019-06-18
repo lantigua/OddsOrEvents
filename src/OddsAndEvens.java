6df67dabb131118e1f85796b21897b33a48e63bc
Pedro L. Lantigua <lantigua.pl@gmail.com>
2019-06-18 14:16:53 -0400

    full code

--- src/OddsAndEvens.java
+++ src/OddsAndEvens.java
@@ -1,5 +1,3 @@
-import com.sun.source.tree.IfTree;
-
 import java.util.*;
 
 public class OddsAndEvens {
@@ -9,16 +7,16 @@ public class OddsAndEvens {
 
         System.out.println("What's your name loser?");
         Scanner input1 = new Scanner(System.in);
-        String name = input1.nextLine();
+        String name = input1.nextLine().toUpperCase();
 
         System.out.println("Hi "+ name +", which do you choose? (O)dds or (E)vens?");
         Scanner input2 = new Scanner(System.in);
         String choose = input2.nextLine().toUpperCase();
 
         if (choose.equals("O")) {
-            System.out.println(name +" has picked odds! The computer will be evens.");
+            System.out.println(name +" has picked \"ODDS\"! The computer will be evens.");
         }else if(choose.equals("E")){
-            System.out.println(name +" has picked evens! The computer will be odds.");
+            System.out.println(name +" has picked \"EVENS\"! The computer will be odds.");
         }else System.out.println(name +" you must pick O or E \n **********************************************");
 
         System.out.println("--------------------------------------------------------------------------------------------");
@@ -40,6 +38,18 @@ public class OddsAndEvens {
 
         if (oddOrEven){
             System.out.println(sum + " is... even!");
-        } System.out.print(sum + " is... odd!");
+            if (choose.equals("E")){
+                System.out.print("That means "+ name + " wins!");
+            } else {
+                System.out.print("That means Computer wins!");
+            }
+        } else {
+            System.out.print(sum + " is... odd!");
+            if (choose.equals("O")){
+                System.out.print("That means " + name + " wins!");
+            } else {
+                System.out.print("That means Computer wins!");
+            }
+        }
     }
 }
